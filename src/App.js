8f0f683f5c4a42012ab3211d013d5db9a9594913
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-01-17 11:40:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-
-import './App.css';
+import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Todo from './components/Todo';
-// import SlickSlider from './components/SlickSlider';
-// import SwiperSlider from './components/SwiperSlider';
-// import SlickCenterMod from './components/SlickCenterMod';
+import SlickSlider from "./components/SlickSlider";
+import SwiperCenterMod from "./components/SwiperCenterMod";
+import SwiperSlider from "./components/SwiperSlider";
+import SlickCenterMod from "./components/SlickCenterMod";
 function App() {
   return (
     <>
-    <Todo/>
-    {/* <SlickSlider/> */}
-    {/* <SlickCenterMod/> */}
-    {/* <SwiperSlider/> */}
+      <SlickSlider />
+      <SlickCenterMod />
+      <SwiperSlider />
+      <SwiperCenterMod />
     </>
   );
 }
